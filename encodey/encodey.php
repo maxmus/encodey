39e786000f073a79097184eb275098f73bbbe402
ensar <maxmus1989@gmail.com>
2014-11-28 15:02:47 +0100

    encodey commited

---
+++ encodey/encodey.php
@@ -0,0 +1,77 @@
+<?php
+function encodey($str,$a ) {
+$strlen = strlen( $str );
+$siz = sizeof($a);
+$res = "";
+$rest = "";
+$t= "";
+$sugl = array();
+$pozsugl = array();
+for( $i = 0; $i < $strlen; $i++ ) {
+    $char = substr( $str, $i, 1 );
+     $t = false;
+	for($j=0; $j < $siz;$j++) {
+		if($a[$j] === $char) {
+		    $t = true;
+		    if($j+1 === $siz) {
+		        $res.= $a[0];
+		        break;
+		    }
+		    else  {
+		        $res .= $a[$j+1];
+		        break;
+		    }
+		}
+	}
+	if(!$t) {
+	   $res.=$char;
+	   array_push($sugl, $char);
+	   array_push($pozsugl, $i);
+	}
+}
+    echo "\n"."sugl ".printing($sugl)."\n" ;
+    echo "\n"."pozsugl ".printing($pozsugl)."\n";
+    $sugl = shifty($sugl);
+    echo "\n"."sugl poslije shifty ".printing($sugl)."\n";
+	echo "\n"."\n"."Rezultat prije ". $res ."\n";
+	echo "\n"."Rezultat nakon svega:" . changeChar($res,$pozsugl, $sugl);
+	}
+	//Sortiranje 
+	function shifty($c) {
+	  $siz = sizeof($c);
+      $tmp = $c[0];
+	  for ($i = 1; $i < $siz; $i++)
+            $c[$i - 1] = $c[$i];
+      $c[$siz - 1] = $tmp;
+      return $c;
+	}
+	//Spajanje samoglasnika sa krajnjom recenicom
+	function changeChar($str, $pozsugl, $sugl) {
+	    $strlen = strlen( $str );
+	    $siz= sizeof($pozsugl);
+	    $rez = "";
+	    for($i = 0; $i <= $strlen; $i++) {
+	         $char = substr( $str, $i, 1 );
+	         for($j=0; $j<$siz;$j++) {
+	             if($i === $pozsugl[$j]) {
+	                $char=$sugl[$j];
+	                break;
+					}
+	         }
+	        $rez.=$char;
+	    }
+	    return $rez;
+	}
+	
+	//Funkcija za printanje
+	function printing($f) {
+	     $siz= sizeof($f);
+	     $t="";
+	     for($k=0; $k <$siz; $k++)
+	        $t.=$f[$k];
+	    echo $t;
+	        
+	}
+	
+
+?>
